9467421b67694052eb9532beead439c1261e1b24
Bhuvanesh-affinsys <115155528+Bhuvanesh-affinsys@users.noreply.github.com>
2022-10-31 11:52:09 +0530

    Delete bank directory

--- bank/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from bank.models import Customer,Account,Ledger
-
-# Register your models here.
-
-admin.site.register(Customer)
-admin.site.register(Account)
-admin.site.register(Ledger)
